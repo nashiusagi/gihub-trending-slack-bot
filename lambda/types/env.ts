2cb5d7d9dda49bc88f5bf274b41da5f361937fb8
nashiusagi <ritotech0226@gmail.com>
2024-05-04 22:19:40 +0900

    [update] process.envの型を拡張

---
+++ lambda/types/env.ts
@@ -0,0 +1,10 @@
+declare module "process" {
+	global {
+		namespace NodeJS {
+			interface ProcessEnv {
+				readonly SLACK_BOT_ACCESS_TOKEN: string;
+				readonly SLACK_BOT_ACCESS_CHANNEL: string;
+			}
+		}
+	}
+}
