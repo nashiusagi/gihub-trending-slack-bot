65ca19fe3d9a6f22756671ad7de5ba9ecd3049c9
nashiusagi <ritotech0226@gmail.com>
2024-05-05 10:53:09 +0900

    [update] httpステータス追加

--- lambda/lib/useFetch.ts
+++ lambda/lib/useFetch.ts
@@ -22,10 +22,25 @@ const handleServerSideErrors = async (res: Response) => {
   switch (res.status) {
     case 400:
       console.error("Bad Request");
-      return new Response("Bad Request", { status: res.status });
+      return new Response("Bad Request", { status: 400 });
+    case 401:
+      console.error("Unauthorized");
+      return new Response("Unauthorized", { status: 401 });
+    case 403:
+      console.error("Forbidden");
+      return new Response("Forbidden", { status: 403 });
     case 404:
       console.error("Not Found");
-      return new Response("Not Found", { status: res.status });
+      return new Response("Not Found", { status: 404 });
+    case 500:
+      console.error("Internal Server Error");
+      return new Response("Internal Server Error", { status: 500 });
+    case 502:
+      console.error("Bad Gateway");
+      return new Response("Bad Gateway", { status: 502 });
+    case 504:
+      console.error("Gateway Timeout");
+      return new Response("Gateway Timeout", { status: 504 });
     default:
       console.error("Unhandled Error");
       return new Response("Unhandled Error", { status: res.status });
